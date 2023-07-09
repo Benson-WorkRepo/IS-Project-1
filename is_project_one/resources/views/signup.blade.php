036475be7a144437f7f1b83820c3920f186283b0
Benson <bmwangi314@gmail.com>
2023-07-09 22:42:42 +0300

    changess

--- is_project_one/resources/views/signup.blade.php
+++ is_project_one/resources/views/signup.blade.php
@@ -4,6 +4,22 @@
 
 <div class = "signupSection">
 <form style = "width: 600px">
+<div class = "firstName">
+    <label for>First Name</label><br>
+    <input type ="text" id="firstName">
+</div>
+<div class = "lastName">
+    <label for>Last Name</label><br>
+    <input type ="text" id="lastName">
+</div>
+<div class = "emailAddress">
+    <label for>Email</label><br>
+    <input type ="text" id="emailAddress">
+</div>
+<div class = "phoneNo">
+    <label for>Phone Number</label><br>
+    <input type ="tel" id="phoneNo">
+</div>
 <div class="idSection">
 <label for="idNumber">ID No</label><br>
   <input type="text" id="idNumber" name="idNumber">
