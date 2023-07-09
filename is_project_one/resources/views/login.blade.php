e741eb1ee761ffcca0ef6e77027f02b95a3eca26
Benson <bmwangi314@gmail.com>
2023-07-09 22:16:28 +0300

    changes

--- is_project_one/resources/views/login.blade.php
+++ is_project_one/resources/views/login.blade.php
@@ -1,3 +1,19 @@
 @extends('layout')
+@section('title', 'Login')
 @section('content')
+
+<div class = "loginSection">
+<form style = "width: 600px">
+<div class="idSection">
+<label for="idNumber">ID No</label><br>
+  <input type="text" id="idNumber" name="idNumber">
+  </div>
+
+  <div class="passwordSection">
+    <label for="inputPassword" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword">
+  </div>
+  <button type="submit" class="submitButton">Submit</button>
+</form>
+    </div>
 @endsection
